118c088a78b485afec7bb0149568483456952de8
Melanie Clark <melaniefclark@gmail.com>
2024-10-10 20:33:34 +0100

    Instructions added and more 8kyu solutions added

--- Python_Solutions/8_kyu/Fundamentals/Return_Negative.py
+++ Python_Solutions/8_kyu/Fundamentals/Return_Negative.py
@@ -1,3 +1,8 @@
+"""
+n this simple assignment you are given a number and have to make it negative.
+But maybe the number is already negative?
+"""
+
 def make_negative( number ):
     if number >0:
         number *= -1
