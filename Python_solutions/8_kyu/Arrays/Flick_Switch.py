40b159f1856376f8f5e301522aecc67c1e8cd239
Melanie Clark <melaniefclark@gmail.com>
2024-10-10 20:43:10 +0100

    8kyu more solutions added

---
+++ Python_solutions/8_kyu/Arrays/Flick_Switch.py
@@ -0,0 +1,25 @@
+"""
+Create a function that always returns True/true for every item in a given list.
+However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+
+Examples
+['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+Notes
+"flick" will always be given in lowercase.
+A list may contain multiple flicks.
+Switch the boolean value on the same element as the flick itself.
+
+"""
+
+def flick_switch(lst):
+    switch = True
+    new_list =[]
+    for item in lst:
+        if item == 'flick':
+            switch = not switch
+        new_list.append(switch)
+    return new_list
