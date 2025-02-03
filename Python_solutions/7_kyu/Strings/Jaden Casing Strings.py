64ce99b470dfb83d1b0dd78a0a5bea168bb249d2
Melanie Clark <melaniefclark@gmail.com>
2025-02-03 20:44:02 +0000

    python 7kyu added

---
+++ Python_solutions/7_kyu/Strings/Jaden Casing Strings.py
@@ -0,0 +1,13 @@
+"""
+For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+"""
+
+def to_jaden_case(string):
+    return " ".join(word.capitalize() for word in string.split())
