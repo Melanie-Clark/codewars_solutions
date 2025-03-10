ef07508894a04fd1b8fc6ca55594db3681b54532
Melanie Clark <melaniefclark@gmail.com>
2025-03-10 20:20:53 +0000

    Python get() added

---
+++ Python_solutions/8_kyu/Strings/bartender_drinks.py
@@ -0,0 +1,24 @@
+"""
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+Input	Output
+"Jabroni"	"Patron Tequila"
+"School Counselor"	"Anything with Alcohol"
+"Programmer"	"Hipster Craft Beer"
+"Bike Gang Member"	"Moonshine"
+"Politician"	"Your tax dollars"
+"Rapper"	"Cristal"
+anything else	"Beer"
+"""
+
+def get_drink_by_profession(param):
+    myDict = {
+        "Jabroni": "Patron Tequila",
+        "School Counselor": "Anything with Alcohol",
+        "Programmer": "Hipster Craft Beer",
+        "Bike Gang Member": "Moonshine",
+        "Politician": "Your tax dollars",
+        "Rapper": "Cristal"
+    }
+
+    return myDict.get(param.title(), "Beer")
